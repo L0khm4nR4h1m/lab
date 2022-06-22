0904a31153e4513779ab758c6862eed7ce788b4a
L0khm4nR4h1m <2020834578@student.uitm.edu.my>
2022-06-22 05:16:41 +0000

    adding userinput.c

---
+++ part1/userinput.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void) {
+	/*This is my secons program in C*/
+	int age;
+	printf("Hi, how old are you?>");
+	scanf("%d", &age);
+	printf("You are %d years old", age);
+	printf("\n\n");
+	return (0);
+}
