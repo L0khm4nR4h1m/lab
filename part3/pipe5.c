74183561119b22879731d1bc02ca09373532de21
L0khm4nR4h1m <104706280+L0khm4nR4h1m@users.noreply.github.com>
2022-06-23 03:08:37 +0800

    Update pipe5.c

--- part3/pipe5.c
+++ part3/pipe5.c
@@ -4,10 +4,49 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-int getPIN() {
-	srand(getpid() + getppid());
-	int secret = 1000 + rand() % 9000;
-	return secret;
+void parentTask(int fd[2]) {
+	close(0);	
+	close(fd[1]);
+	dup(fd[0]);
+	
+	int num;
+	int check = 0;
+	read(fd[0], &num, sizeof(num));
+	
+	printf("Waiting for number...\n");
+	wait(NULL);
+
+  	if (num == 0 || num == 1) {
+    		check = 1;
+	}
+
+  	for (int i = 2; i <= num / 2; i++) {
+
+    		if (num % i == 0) {
+      			check = 1;
+      			break;
+    		}
+  	}
+
+  	if (check == 0) {
+    		printf("%d is a prime number.", num);
+	}
+  	else {
+    		printf("%d is not a prime number.", num);
+	}
+}
+
+void childTask(int fd[2]) {
+	int n;
+	printf("Enter a number > ");
+	scanf("%d", &n);
+	
+	close(1);
+	close(fd[0]);
+	dup(fd[1]);
+
+	write(fd[1], &n, sizeof(n));
+	exit(EXIT_SUCCESS);
 }
 
 int main(void) {
@@ -16,26 +55,10 @@ int main(void) {
 	pid_t pid = fork();
 
 	if (pid > 0) {
-		close(0);	
-		close(fd[1]);
-		dup(fd[0]);
-
-		int secretNumber;
-		size_t readBytes = read(fd[0], &secretNumber, sizeof(secretNumber));
-
-		printf("Waiting for PIN...\n");
-		wait(NULL);
-		printf("Bytes read: %ld\n", readBytes);
-		printf("PIN: %d\n", secretNumber);
+		parentTask(fd);
 	}
 	else if (pid == 0) {
-		close(1);
-		close(fd[0]);
-		dup(fd[1]);
-
-		int secret = getPIN();
-		write(fd[1], &secret, sizeof(secret));
-		exit(EXIT_SUCCESS);
+		childTask(fd);
 	}
 
 	return EXIT_SUCCESS;
