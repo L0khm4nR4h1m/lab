6fe26bb98d0d2a138bb77ce44630e9b5a188babd
L0khm4nR4h1m <104706280+L0khm4nR4h1m@users.noreply.github.com>
2022-06-25 20:59:14 +0800

    Update chatclient.c

--- part4/chatclient.c
+++ part4/chatclient.c
@@ -1,53 +1,50 @@
 #include<stdio.h>
 #include<sys/socket.h>
-#include<arpa/inet.h>	//inet_addr
-#include<string.h>
+#include<sys/types.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+#include<unistd.h>
 
-int main(int argc , char *argv[])
-{
-	int socket_desc;
+int main(int argc, char*argv[]) {
+
+	int a,socket_desc;
+	char str[20],str2[20];
 	struct sockaddr_in server;
-	char *message , server_reply[2000];
 	
-	//Create socket
-	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-	if (socket_desc == -1)
-	{
+	server.sin_addr.s_addr=inet_addr("192.168.1.222");
+	server.sin_family=AF_INET;
+	server.sin_port=htons(8888);
+	socket_desc=socket(AF_INET,SOCK_STREAM,0);
+	
+	if(socket_desc < 0) {
 		printf("Could not create socket");
-	}
-		
-	server.sin_addr.s_addr = inet_addr("192.168.1.222"); //Please enter the ip address of your Server VM
-	server.sin_family = AF_INET;
-	server.sin_port = htons(8888);
-
-	//Connect to remote server
-	if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0)
-	{
-		puts("connect error");
 		return 1;
 	}
 	
-	puts("Connected \n");
-
-	//Send some data
-	message = "connect";
-	if( send(socket_desc , message , strlen(message) , 0) < 0)
-	{
-		puts("Send failed");
+	if(connect(socket_desc,(struct sockaddr*)&server, sizeof(server)) < 0) {
+		printf("Connect error");
 		return 1;
 	}
-	puts("Data Send\n");
-                    //Receive a reply from the server
 	
-	while(1) {
-	if( recv(socket_desc, server_reply , 2000 , 0) < 0) {
-			puts("recv failed");
-			return 1;
-		}
-		puts("Reply received\n");
+	printf("\nclient connected to 192.168.1.222");
+	printf("\nCLIENT");
+	scanf("%s", &str);
 	
-		puts(server_reply);
+	if(write(socket_desc, str, sizeof(str)) < 0) {
+		printf("\n Message cannot be send");
 	}
-
+	
+	do {
+		listen(socket_desc, 3);
+		read(socket_desc, str2,sizeof(str2));
+		printf("\nserver msg:%s", str2);
+		printf("\nclient msg:");
+		scanf("%s", &str);
+		a=strcmp(str2, "BYE");
+		write(socket_desc, str2, sizeof(str2));
+	} 
+	while(a!=0);
+	
+	close(socket_desc);
 	return 0;
 }
